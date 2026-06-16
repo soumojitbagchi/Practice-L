f19163b54fdb3987fc796d501506b1a4359cb929
soumojitbagchi <soumojitbagchi001@gmail.com>
2026-06-16 10:41:21 +0530

    enhance signup and aboutUs components; improve form handling, add password visibility toggle, and update layout styles

--- frontend/userinterface/src/firstInterface/aboutUs.jsx
+++ frontend/userinterface/src/firstInterface/aboutUs.jsx
@@ -1,24 +1,51 @@
 import React from "react";
 import { motion } from "framer-motion";
-
+import "./Container.css";
 const aboutUs = () => {
+  const text = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, animi tenetur. Dolorem repudiandae animi facilis voluptatibus, temporibus saepe aspernatur? Sunt animi dolor doloribus deleniti perferendis eius placeat quisquam nostrum labore rerum et doloremque explicabo facilis, blanditiis reprehenderit tempora inventore repellat eaque molestias sequi fugiat! Velit sed facilis hic blanditiis consequuntur totam a recusandae praesentium dolores delectus. Vero iure molestiae, nam nisi corrupti dolorem quo porro ipsam! Tempora, possimus vel! Aliquam ratione atque quasi minima commodi recusandae architecto itaque obcaecati, blanditiis iusto neque quidem non ab saepe illo! Ratione voluptas impedit a, doloremque similique nihil, praesentium incidunt dolor voluptatibus deleniti nulla nemo doloribus repellendus quis ducimus maiores! Sapiente mollitia rem inventore. Eligendi eos saepe consequatur totam hic, porro tempore veniam repellat dolores amet, optio, mollitia vitae doloremque perferendis cupiditate aperiam et fugiat ab. Eum error est voluptate perferendis rem cumque voluptates natus cupiditate eligendi. Ab error porro beatae veniam adipisci praesentium iure voluptate exercitationem, saepe, nulla odit, a quisquam magnam. Odio mollitia error voluptatem numquam,
+  tempore animi cumque, saepe velit voluptates hic suscipit. Aspernatur consectetur praesentium quas omnis quia vel unde ea facere eveniet, ratione ut ipsam. Delectus sapiente officiis obcaecati nemo neque quaerat placeat. Nisi a pariatur numquam voluptatum, voluptates rerum sequi nesciunt soluta eaque, ipsam, illo animi est temporibus dolorum nostrum quo exercitationem! Itaque labore, doloremque nisi voluptates accusantium blanditiis, sed rem totam atque, ex inventore. Dolor recusandae harum, enim, quisquam adipisci qui maiores voluptas, vero id quam dolore porro! Nulla eum officiis neque consequuntur possimus maxime quam illum, animi hic nostrum quibusdam. Reprehenderit, quos eligendi! Eius ad ea explicabo aliquam consequatur beatae. Eius, cupiditate. Aut quam amet iusto numquam molestiae exercitationem suscipit repudiandae placeat cumque ratione molestias, ex corrupti voluptatibus sapiente nisi sed itaque architecto? Labore ipsum veritatis quaerat, facilis ea eum commodi debitis saepe. Sequi unde, mollitia omnis corrupti error neque blanditiis similique nisi quidem adipisci impedit repellat! Impedit animi praesentium culpa expedita voluptate aut reprehenderit accusantium omnis. Facere quaerat optio, quia esse incidunt laudantium est molestias qui, laborum, provident similique quasi! Assumenda obcaecati quisquam saepe commodi iusto sit, quasi non ea quos ullam molestiae nulla quibusdam architecto quis officia sequi, sed et repudiandae similique animi at ut minima? Omnis laudantium laboriosam vel deleniti iste minima nihil excepturi inventore mollitia tenetur eos voluptas, quod, architecto aut placeat doloribus quo, veniam beatae praesentium quibusdam autem! Necessitatibus vel recusandae laborum illo consequuntur quasi blanditiis nemo dolorum, itaque praesentium ipsam modi totam voluptatum ea unde eum dicta, placeat similique id, esse iste deserunt animi natus odit. Laboriosam iure doloremque recusandae laudantium libero sed quibusdam cupiditate voluptatem odit. Rem, nulla velit modi a facere consequatur ut fugiat, minima nemo officia repudiandae dignissimos aut expedita ad excepturi. Animi tempore id at ipsum, odio debitis vel. Quisquam eaque rem, minus nemo velit corrupti suscipit in! Aliquam similique, eveniet magni provident ab minima vero, illo blanditiis in maxime dolorum impedit obcaecati perspiciatis perferendis ratione odit. Itaque in nostrum vel. Officiis animi reiciendis facilis perferendis. Ratione saepe cupiditate nisi similique ab, iure vel cumque modi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate corrupti quaerat quo quis. Assumenda eaque neque eveniet repudiandae amet tempore, consectetur quaerat perferendis obcaecati recusandae ullam soluta libero adipisci provident aliquam sunt placeat mollitia quidem, veniam officia laborum perspiciatis rem molestiae odio. Quae, ratione repudiandae quis eos tempora, ad odio porro autem impedit officiis quia doloremque atque harum explicabo placeat magnam quidem, sint nihil perspiciatis suscipit amet magni dolore provident illum? Cum qui minima in temporibus veritatis alias ipsa consequatur quibusdam quidem nulla corrupti autem vel voluptates animi, consequuntur tempora cumque. Nostrum reiciendis eum obcaecati ipsa voluptate consequuntur tempora! Sit sequi placeat ullam enim ipsam est facere, vitae neque consectetur perspiciatis sint modi natus! Hic itaque fuga eos nemo, doloremque voluptates sequi vitae quam dolorem repellat nulla optio enim excepturi incidunt aspernatur inventore. Voluptate ex dignissimos impedit placeat ipsum. Quo asperiores dignissimos beatae provident doloribus porro corrupti voluptatibus enim aspernatur! Vitae commodi assumenda possimus nisi ratione quas perferendis impedit ea ex consequatur, voluptate eaque, harum laborum nam nemo beatae aliquid dolor quam veritatis ipsam quasi totam! Rerum facere cupiditate sapiente ullam pariatur quis sint illo sed est officiis. Praesentium, mollitia. Ratione ex ut aspernatur natus accusantium odio consectetur quos nam atque fuga magni, molestias numquam, sapiente recusandae commodi maxime hic excepturi animi suscipit voluptatem fugiat. Dolores neque nostrum quod aliquid quasi dolorum, quas totam magnam cupiditate aut voluptates id repudiandae corrupti veritatis magni architecto maxime nobis. Hic dicta veritatis, explicabo harum et nemo, voluptatibus ex natus aliquid ab, dolores accusantium eius earum iure? Modi voluptatem autem iure molestiae nostrum totam eaque assumenda earum optio voluptatibus aliquid quos magnam, at, aut nemo nesciunt recusandae aspernatur architecto, natus quod tenetur. Suscipit magni odio ducimus dignissimos deleniti exercitationem dolorum iusto ipsum rem, culpa minima incidunt? Consequatur, excepturi! Eveniet, dolore. Aspernatur iusto dolorem quis beatae ratione, consequatur odio incidunt velit ad distinctio, vero debitis quaerat molestiae tenetur eligendi. Facere dolorem quisquam enim sit atque laborum animi praesentium blanditiis, iste fuga doloremque id neque cumque voluptas nihil labore, eligendi maiores quos delectus eos! Praesentium est iusto molestias illum quaerat perspiciatis adipisci repellendus sint unde quas esse aspernatur hic autem, odit odio nesciunt consequatur nulla dolorum pariatur distinctio mollitia harum doloremque. Exercitationem ea voluptas dolore, repellendus et eos architecto dolorum natus ducimus perferendis mollitia, id, sit dolorem odio incidunt? Quisquam provident neque iste ut, laborum eos magni tenetur reiciendis cum. Itaque omnis id numquam voluptas odit deserunt libero voluptatibus. Ut officia minima culpa, rem sit odio nulla nesciunt, consectetur accusantium perferendis nihil? Totam labore dolores dolor velit veritatis aut praesentium nulla laborum neque atque id sunt ut, magni debitis ipsam? Iusto tempore earum sed rerum et iste quos. Nemo officiis, vitae voluptatibus vero placeat dolore a ipsa sunt eaque, nostrum quasi voluptas, cum accusantium quod dignissimos reprehenderit expedita consectetur ab aliquid asperiores quidem aspernatur repellat dolorum. Ex, officiis voluptates! Laborum autem rem fugit aperiam modi, distinctio eos voluptatem illum tenetur, nobis asperiores nihil magnam laudantium placeat voluptatibus temporibus. Non autem perspiciatis cupiditate nesciunt et ab possimus! `;
   return (
-    <div>
-      aboutUs
-      <motion.button
+    <div className="flex flex-col gap-5">
+      <h1 className="font-serif text-6xl text-center underline  mb-5">About us</h1>
+
+      <div className="flex flex-row p-5">
+        <p className="w-[76vw] h-full p-20 ">
+          {text.split(" ").map((char, idx) => (
+            <span
+              key={idx}
+              className="inline-block transition-transform duration-200 ease-out hover:scale-[1.4] cursor-pointer text-2xl text-center"
+            >
+              {char==="."?<br/>:char}
+            </span>
+          ))}
+        </p>
+        <div className="about-img">
+          <img
+            src="https://images.unsplash.com/photo-1780445333334-31c3017e6e84?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <img
+            src="https://plus.unsplash.com/premium_photo-1673860367181-2f5d4e27add4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
+      </div>
+
+      {/* <motion.button
         transition={{
           backgroundColor: ["white", "black"],
           color: ["black", "white"],
           duration: 3,
           repeat: Infinity,
         }}
-        whileHover={{ scale: 0.95 ,
+        whileHover={{
+          scale: 0.95,
           backgroundColor: ["black"],
           color: ["white"],
         }}
       >
         hello
-      </motion.button>
+      </motion.button> */}
     </div>
   );
 };
--- frontend/userinterface/src/firstInterface/signup.jsx
+++ frontend/userinterface/src/firstInterface/signup.jsx
@@ -3,20 +3,27 @@ import BackButton from "./BackButton";
 import SignupThreeBackground from "./components/SignupThreeBackground";
 import { motion } from "framer-motion";
 import "./Container.css";
+import { VscEyeClosed } from "react-icons/vsc";
+import { VscEye } from "react-icons/vsc";
 import { useState } from "react";
 
 const signup = () => {
-  const [email, setEmail] = useState('')
-  const [name, setname] = useState('')
-  const [password, setPassword] = useState()
-   const submitHandeler = (e) => {
+  const [email, setEmail] = useState("");
+  const [name, setname] = useState("");
+  const [password, setPassword] = useState();
+  const [showPassword, setShowPassword] = useState(false);
+  const submitHandeler = (e) => {
     e.preventDefault();
     setEmail("");
     setPassword("");
   };
+  const previewChanger = (e) => {
+    setShowPassword((prev) => !prev);
+    e.preventDefault()
+  };
   return (
     <div>
-      <SignupThreeBackground/>
+      <SignupThreeBackground />
       <BackButton />
       <form
         action=""
@@ -30,7 +37,7 @@ const signup = () => {
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="enter your registered email"
+            placeholder="enter your name  "
           />
           <input
             type="email"
@@ -38,16 +45,21 @@ const signup = () => {
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="enter your registered email"
+            placeholder="enter email"
           />
-          <input
-            type="password"
-            className="logIn-input"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="enter password"
-          />
+          <div className="logIn-input flex justify-between">
+            <input
+              className=" text-center justify-center w-[43vw] outline-0"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="enter password"
+            ></input>
+            <motion.button onClick={previewChanger} className=" text-3xl">
+              {showPassword ? <VscEye /> : <VscEyeClosed />}
+            </motion.button>
+          </div>
         </div>
 
         <motion.button
